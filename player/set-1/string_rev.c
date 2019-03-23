81ce230f486f4aaefd47af2d2ef991211926a00a
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2019-03-23 10:58:20 +0530

    Create string_rev.c

---
+++ player/set-1/string_rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	char str[10];
+	char copy;
+	int left,right,index=0;
+     scanf("%s",str);
+	while(str[++index]);
+         	//printf("%d\n",index);
+    right=index-1,left=0;
+	while(left<right)
+	   {
+	   	copy=str[left];
+	   	str[left]=str[right];
+	   	str[right]=copy;
+	   	left++;
+	   	right--;
+	   }	
+	printf("%s",str);
+	
+	return 0;
+}
